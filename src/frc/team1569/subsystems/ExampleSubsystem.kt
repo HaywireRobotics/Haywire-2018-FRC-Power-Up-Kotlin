11b8580dfc13648ea50dbdca90c57e5535c0085d
zPaw <metaherobrine1@gmail.com>
2018-10-29 20:57:36 -0600

    Init

---
+++ src/frc/team1569/subsystems/ExampleSubsystem.kt
@@ -0,0 +1,16 @@
+package frc.team1569.subsystems
+
+import edu.wpi.first.wpilibj.command.Subsystem
+
+/**
+ *
+ */
+class ExampleSubsystem: Subsystem() {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  override fun initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
